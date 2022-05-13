d7de2f3a87bfc00302dd3a076f15808a07400c6a
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-05-13 16:18:11 +0600

    table added

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,3 +1,4 @@
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
 import { Hit } from "./interface";
 
@@ -9,12 +10,61 @@ const Table: React.FC<{ data: Hit[] | undefined }> = ({ data }) => {
       setPosts(data);
     }
   });
-  console.log("postsTable", posts);
+  //console.log("postsTable", posts);
+
+  const columns: GridColDef[] = [
+    {
+      field: "title",
+      headerName: "Title",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "url",
+      headerName: "URL",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "created_at",
+      headerName: "Created At",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "author",
+      headerName: "Author",
+      width: 150,
+      editable: true,
+    },
+  ];
+
+  const rows: {
+    title: string;
+    url: string;
+    created_at: Date;
+    author: string;
+  }[] = [];
+  // eslint-disable-next-line array-callback-return
+  if (posts) {
+    posts?.map((i, j) => {
+      rows[j].title = i.title;
+      rows[j].url = i.url;
+      rows[j].created_at = i.created_at;
+      rows[j].author = i.author;
+    });
+  }
+
   return (
-    <div>
-      {posts?.map((i) => {
-        return <h6>{i.title}</h6>;
-      })}
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
     </div>
   );
 };
